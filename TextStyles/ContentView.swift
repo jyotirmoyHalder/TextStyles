e77c17ffd939b226196092d3b8ee3541072e845d
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2026-07-27 21:29:51 +0600

    Initial Commit

---
+++ TextStyles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextStyles
+//
+//  Created by jyotirmoy_halder on 27/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextStyles/TextStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextStylesApp.swift
+//  TextStyles
+//
+//  Created by jyotirmoy_halder on 27/7/26.
+//
+
+import SwiftUI
+
+@main
+struct TextStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
